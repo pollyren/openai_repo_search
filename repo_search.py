a2c938671af320525b5373bec358fc237b901574
Polly Ren <renpollyf@gmail.com>
2023-10-27 16:34:26 -0500

    prevent overwriting and remove comments

--- repo_search.py
+++ repo_search.py
@@ -20,30 +20,22 @@ query = sys.argv[1]
 url = f'https://api.github.com/search/code?q={query}&ref=advsearch&per_page=100&page='
 
 repos = {}
-# id_list = []
 with requests.session() as s:
     for page in range(1, 11):
         while True: # sleep the timeout
             res = s.get(url + str(page), headers=HEADERS)
             if res.status_code != 200:
-                # print('---------break at '+str(page)+'---'+str(res.status_code))
                 time.sleep(5)
                 continue
             break   
         res_json = json.loads(str(res.content, 'utf-8'))
         res_items = res_json['items']
         for repo in res_items:
-            # score = repo['score']
-            # repo_name = repo['repository']['full_name']
+            repo_name = repo['repository']['full_name']
             repo_id = repo['repository']['id']
-            repo_url = repo['repository']['html_url']
+            # repo_url = repo['repository']['html_url']
             repo_path = repo['path']
-            repos[repo_id] = (repo_url, repo_path)
-            # if repo_id not in id_list:
-            #     repo_dict[repo_id] = {'name':repo_name, 'url':repo_url}
-            #     id_list.append(repo_id)
-            # else:
-            #     print(f'Duplicated: {repo_name}')
+            repos[repo_id + repo_path] = (repo_name, repo_path)
 
 print(f'{len(list(repos.keys()))} repositories found.')
 print('writing to file...', end='')
